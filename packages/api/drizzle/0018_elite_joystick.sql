0f070aeb2b6c7635f454af28002e8092e72d82cf
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2025-07-27 09:04:21 +0100

    fix(api):  explicitly cast availability column type

--- packages/api/drizzle/0018_elite_joystick.sql
+++ packages/api/drizzle/0018_elite_joystick.sql
@@ -2,5 +2,5 @@ ALTER TABLE "catalog_items" ALTER COLUMN "categories" SET NOT NULL;--> statement
 ALTER TABLE "catalog_items" ALTER COLUMN "images" SET DEFAULT '[]'::jsonb;--> statement-breakpoint
 ALTER TABLE "catalog_items" ALTER COLUMN "product_url" SET NOT NULL;--> statement-breakpoint
 CREATE TYPE "availability" AS ENUM ('in_stock', 'out_of_stock', 'preorder');--> statement-breakpoint
-ALTER TABLE "catalog_items" ALTER COLUMN "availability" SET DATA TYPE availability;--> statement-breakpoint
+ALTER TABLE "catalog_items" ALTER COLUMN "availability" SET DATA TYPE availability USING "availability"::availability;--> statement-breakpoint
 ALTER TABLE "catalog_items" DROP COLUMN "url";
