898ad67f097d2a8ef1a97398a4844a00827cd302
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2025-08-10 23:39:48 +0100

    fix(api): use drizzle neon-serveless adapter because it supports transactions

--- packages/api/src/db/index.ts
+++ packages/api/src/db/index.ts
@@ -1,19 +1,19 @@
-import { neon } from '@neondatabase/serverless';
+import { Pool } from '@neondatabase/serverless';
 import * as schema from '@packrat/api/db/schema';
 import type { Env } from '@packrat/api/types/env';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 import type { Context } from 'hono';
 import { env as honoEnv } from 'hono/adapter';
 
 // Create SQL client with Neon for Hono contexts
 export const createDb = (c: Context) => {
   const { NEON_DATABASE_URL } = honoEnv<Env>(c);
-  const sql = neon(NEON_DATABASE_URL);
-  return drizzle(sql, { schema });
+  const pool = new Pool({ connectionString: NEON_DATABASE_URL });
+  return drizzle(pool, { schema });
 };
 
 // Create SQL client with Neon for queue workers
 export const createDbClient = (env: Env) => {
-  const sql = neon(env.NEON_DATABASE_URL);
-  return drizzle(sql, { schema });
+  const pool = new Pool({ connectionString: env.NEON_DATABASE_URL });
+  return drizzle(pool, { schema });
 };
