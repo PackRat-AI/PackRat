2444de51e95dc91fcdb9577dd47d85008ce4dec7
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2025-08-19 11:36:51 -0600

    🎨 lint

--- apps/expo/features/catalog/components/ItemLinks.tsx
+++ apps/expo/features/catalog/components/ItemLinks.tsx
@@ -1,7 +1,7 @@
 import { Text } from '@packrat/ui/nativewindui';
 import { Icon } from '@roninoss/icons';
-import { useColorScheme } from 'expo-app/lib/hooks/useColorScheme';
 import type { CatalogItem } from 'expo-app/features/catalog/types';
+import { useColorScheme } from 'expo-app/lib/hooks/useColorScheme';
 import { Linking, TouchableOpacity, View } from 'react-native';
 
 type ItemLinksProps = {
