5198e6d041c424b3d8d5ab673cee3f60b71a13f4
Anmol Verma <anmolverma2810@gmail.com>
2025-08-22 18:45:47 +0530

    revert changes suggested

--- apps/expo/components/initial/ExpandableText.tsx
+++ apps/expo/components/initial/ExpandableText.tsx
@@ -11,14 +11,7 @@ export function ExpandableText({
 }) {
   const [expanded, setExpanded] = useState(false);
   const MIN_WIDTH = 60;
-  expandThreshold = 60,
-}: {
-  text: string;
-  numberOfLines?: number;
-  expandThreshold?: number;
-}) {
-  const [expanded, setExpanded] = useState(false);
-  const MIN_WIDTH = 60;
+  const expandThreshold = 60;
 
   return (
     <View
