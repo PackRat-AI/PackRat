6a181c7e57badc0c6a5e012858542a614cc22eab
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2025-09-15 09:57:21 +0100

    fix(expo): ensure items are deleted when deleting a pack

--- apps/expo/features/packs/hooks/useDeletePack.ts
+++ apps/expo/features/packs/hooks/useDeletePack.ts
@@ -1,10 +1,11 @@
-import { packsStore } from 'expo-app/features/packs/store';
+import { getPackItems, packItemsStore, packsStore } from 'expo-app/features/packs/store';
 import { useCallback } from 'react';
 
 export function useDeletePack() {
   const deletePack = useCallback((id: string) => {
     // Soft delete by setting deleted flag
     // @ts-ignore: Safe because Legend-State uses Proxy
+    getPackItems(id).forEach((item) => packItemsStore[item.id].deleted.set(true));
     packsStore[id].deleted.set(true);
   }, []);
 
