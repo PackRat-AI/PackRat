930dcb94bf658ec81a13d2a90de25e4d4d5756b3
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2025-10-28 10:48:00 +0100

    chore: bump version to v2.0.13

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -37,7 +37,7 @@ export default (): ExpoConfig =>
     {
       name: getAppName(),
       slug: 'packrat',
-      version: '2.0.12',
+      version: '2.0.13',
       scheme: 'packrat',
       web: {
         bundler: 'metro',
