278bb2c4999b1f2929ef3ac469561b6d75725d30
Andrew Bierman <94939237+andrew-bierman@users.noreply.github.com>
2026-02-12 15:21:51 -0700

    🚚 Rename BOARD_BUCKET binding to SWARMBOARD_BUCKET

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,14 +1,14 @@
 import { createApp } from "./app";
 
 interface Env {
-	BOARD_BUCKET: R2Bucket;
+	SWARMBOARD_BUCKET: R2Bucket;
 	API_KEY: string;
 }
 
 // CF Worker entry point
 export default {
 	async fetch(request: Request, env: Env): Promise<Response> {
-		const app = createApp(env.BOARD_BUCKET, env.API_KEY);
+		const app = createApp(env.SWARMBOARD_BUCKET, env.API_KEY);
 		return app.handle(request);
 	},
 };
