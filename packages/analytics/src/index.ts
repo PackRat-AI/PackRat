fbdaf672f89428efae6d877b1cd1f7344d4e447a
Andrew Bierman <abbierman101@gmail.com>
2026-03-12 23:44:02 -0600

    📦 chore(analytics): export EntityResolver and SpecParser from index

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -1,6 +1,8 @@
 export * from './core/cache-metadata';
 export * from './core/constants';
 export { PackRatEngine } from './core/engine';
+export { EntityResolver } from './core/entity-resolver';
 export { LocalCacheManager } from './core/local-cache';
 export { QueryBuilder, SQLFragments } from './core/query-builder';
+export { SpecParser } from './core/spec-parser';
 export * from './types/index';
