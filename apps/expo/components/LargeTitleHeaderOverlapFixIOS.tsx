aed3e3b6c538a34bbc122cab1bcf3982bfeba577
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2026-04-24 16:13:52 +0100

    fix(expo): enhance LargeTitleHeaderOverlapFixIOS to handle children and improve layout on iOS

--- apps/expo/components/LargeTitleHeaderOverlapFixIOS.tsx
+++ apps/expo/components/LargeTitleHeaderOverlapFixIOS.tsx
@@ -1,11 +1,25 @@
 import { Platform, SafeAreaView, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export const LargeTitleHeaderOverlapFixIOS = () => {
-  if (Platform.OS === 'android') return null;
+export const LargeTitleHeaderOverlapFixIOS = ({ children }: { children?: React.ReactNode }) => {
+  if (Platform.OS === 'android') {
+    if (!children) {
+      return null;
+    } else {
+      return children;
+    }
+  }
+
+  const insets = useSafeAreaInsets();
 
   return (
-    <SafeAreaView>
-      <View className="h-[0.5]" />
+    <SafeAreaView
+      {...(children
+        ? { className: 'flex-1 bg-background', style: { paddingTop: insets.top } }
+        : {})}
+    >
+      {children}
+      {!children && <View className="h-[0.5]" />}
     </SafeAreaView>
   );
 };
