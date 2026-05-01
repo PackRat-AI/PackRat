44924a502beaca6db601d903ad66e21e5daf1da4
Andrew Bierman <abbierman101@gmail.com>
2026-05-01 17:53:05 -0600

    chore: scaffold @packrat/domain package
    Adds packages/domain with package.json, tsconfig, and barrel index files
    for packs, catalog, trips, and pack-templates sub-packages. Registers
    @packrat/domain/* paths in the root tsconfig.

---
+++ packages/domain/src/index.ts
@@ -0,0 +1,4 @@
+export * from './catalog/index';
+export * from './pack-templates/index';
+export * from './packs/index';
+export * from './trips/index';
---
+++ packages/domain/src/pack-templates/index.ts
@@ -0,0 +1,8 @@
+export type {
+  PackTemplate,
+  PackTemplateInput,
+  PackTemplateInStore,
+  PackTemplateItem,
+  PackTemplateItemInput,
+  WeightUnit,
+} from './types';
